03d65d1aeb685d5b037eba0073766b4ca435a5a1
SplashAni <106507600+SplashAni@users.noreply.github.com>
2023-02-20 16:39:00 +0200

    :trollface: progress

--- src/main/java/random/meteor/Modules/misc/CapesBypass.java
+++ src/main/java/random/meteor/Modules/misc/CapesBypass.java
@@ -1,13 +1,7 @@
-/*
- * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
- * Copyright (c) Meteor Development.
- */
-
 package random.meteor.Modules.misc;
 
 import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.misc.MeteorIdentifier;
 import meteordevelopment.orbit.EventHandler;
@@ -17,27 +11,30 @@ import random.meteor.Main;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.io.File.separator;
 
 public class CapesBypass extends Module {
     private static final Map<String, Cape> TEXTURES = new HashMap<>();
 
     private static final List<Cape> TO_REGISTER = new ArrayList<>();
-    private static final List<Cape> TO_REMOVE = new ArrayList<>();
 
     public CapesBypass() {
-        super(Main.MISC,"CapeBypass","saves 10 euroes");
-        // Load cape textures from local files
-        File capesDir = new File(MeteorClient.FOLDER, "capes" + "cape.png");
-        if (!capesDir.exists() || !capesDir.isDirectory()) {
-            capesDir.mkdirs();
-        }
+        super(Main.MISC, "CapeBypass", "Saves 10 euros.");
 
-        for (File file : capesDir.listFiles()) {
-            String name = file.getName();
-            if (!name.endsWith(".png")) continue;
-            name = name.substring(0, name.lastIndexOf('.'));
-            TEXTURES.put(name, new Cape(name, file));
+        String capePath = MeteorClient.FOLDER + separator + "capes" + separator + "cape.png";
+
+        File capeFile = new File(capePath);
+        if (!capeFile.exists() || !capeFile.isFile()) {
+            MeteorClient.LOG.error("Cape fil is missing" + capePath);
+            this.toggle();
+        } else {
+            Cape cape = new Cape(capeFile);
+            TEXTURES.put(capePath, cape);
         }
 
         MeteorClient.EVENT_BUS.subscribe(CapesBypass.class);
@@ -49,28 +46,15 @@ public class CapesBypass extends Module {
             for (Cape cape : TO_REGISTER) cape.register();
             TO_REGISTER.clear();
         }
-
-        synchronized (TO_REMOVE) {
-            for (Cape cape : TO_REMOVE) {
-                TEXTURES.remove(cape.name);
-                TO_REGISTER.remove(cape);
-            }
-
-            TO_REMOVE.clear();
-        }
     }
 
-
     private class Cape extends MeteorIdentifier {
-        private final String name;
-
+        private final File file;
         private NativeImage img;
 
-        public Cape(String name, File file) {
-            super("capes/" + name);
-
-            this.name = name;
-
+        public Cape(File file) {
+            super(file.getAbsolutePath());
+            this.file = file;
             try {
                 img = NativeImage.read(String.valueOf(file));
             } catch (IOException e) {
