0228e9fe80782d22546a15368965ae01297ff610
SplashAni <106507600+SplashAni@users.noreply.github.com>
2023-02-24 17:31:42 +0200

    :aries: Added delay between armor swop

--- src/main/java/random/meteor/Modules/misc/LeftClickArmor.java
+++ src/main/java/random/meteor/Modules/misc/LeftClickArmor.java
@@ -7,7 +7,9 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import random.meteor.Main;
+import random.meteor.Utils.MiscUtils;
 
+import static random.meteor.Utils.MiscUtils.lastSwapTime;
 import static random.meteor.Utils.MiscUtils.shouldTrigger;
 
 public class LeftClickArmor extends Module {
@@ -30,24 +32,21 @@ public class LeftClickArmor extends Module {
                 ItemStack helmet = mc.player.getInventory().armor.get(3);
                 mc.player.getInventory().armor.set(3, pain);
                 mc.player.setStackInHand(mc.player.getActiveHand(), helmet);
-            }
-            else if (pain.getItem() == Items.LEATHER_CHESTPLATE || pain.getItem() == Items.CHAINMAIL_CHESTPLATE ||
+            } else if (pain.getItem() == Items.LEATHER_CHESTPLATE || pain.getItem() == Items.CHAINMAIL_CHESTPLATE ||
                     pain.getItem() == Items.IRON_CHESTPLATE || pain.getItem() == Items.GOLDEN_CHESTPLATE ||
                     pain.getItem() == Items.DIAMOND_CHESTPLATE || pain.getItem() == Items.NETHERITE_CHESTPLATE) {
 
                 ItemStack chestplate = mc.player.getInventory().armor.get(2);
                 mc.player.getInventory().armor.set(2, pain);
                 mc.player.setStackInHand(mc.player.getActiveHand(), chestplate);
-            }
-            else if (pain.getItem() == Items.LEATHER_LEGGINGS || pain.getItem() == Items.CHAINMAIL_LEGGINGS ||
+            } else if (pain.getItem() == Items.LEATHER_LEGGINGS || pain.getItem() == Items.CHAINMAIL_LEGGINGS ||
                     pain.getItem() == Items.IRON_LEGGINGS || pain.getItem() == Items.GOLDEN_LEGGINGS ||
                     pain.getItem() == Items.DIAMOND_LEGGINGS || pain.getItem() == Items.NETHERITE_LEGGINGS) {
 
                 ItemStack leggings = mc.player.getInventory().armor.get(1);
                 mc.player.getInventory().armor.set(1, pain);
                 mc.player.setStackInHand(mc.player.getActiveHand(), leggings);
-            }
-            else if (pain.getItem() == Items.LEATHER_BOOTS || pain.getItem() == Items.CHAINMAIL_BOOTS ||
+            } else if (pain.getItem() == Items.LEATHER_BOOTS || pain.getItem() == Items.CHAINMAIL_BOOTS ||
                     pain.getItem() == Items.IRON_BOOTS || pain.getItem() == Items.GOLDEN_BOOTS ||
                     pain.getItem() == Items.DIAMOND_BOOTS || pain.getItem() == Items.NETHERITE_CHESTPLATE) {
 
@@ -55,6 +54,7 @@ public class LeftClickArmor extends Module {
                 mc.player.getInventory().armor.set(0, pain);
                 mc.player.setStackInHand(mc.player.getActiveHand(), boots);
             }
+            lastSwapTime = System.currentTimeMillis(); // update
         }
     }
 }
--- src/main/java/random/meteor/Utils/MiscUtils.java
+++ src/main/java/random/meteor/Utils/MiscUtils.java
@@ -13,16 +13,19 @@ import static java.io.File.separator;
 import static meteordevelopment.meteorclient.MeteorClient.mc;
 
 public class MiscUtils {
-    static String appData = System.getenv("APPDATA");
+
+    public static long lastSwapTime = 0;
+
 
     public static boolean shouldTrigger() {
         if (GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_RIGHT) != GLFW.GLFW_PRESS) {
             return false;
         }
-
         if (mc.currentScreen != null) {
             return false;
         }
-        return true;
+        long currentTime = System.currentTimeMillis();
+        long timeSinceLastSwap = currentTime - lastSwapTime;
+        return timeSinceLastSwap >= 1000; // 1 second delay
     }
 }
