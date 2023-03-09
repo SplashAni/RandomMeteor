7ea7a17ae7e3032d2d0199367fdd42e6b56ff639
SplashAni <106507600+SplashAni@users.noreply.github.com>
2023-03-09 19:33:12 +0200

    :weary: actually no

--- src/main/java/random/meteor/Utils/MiscUtils.java
+++ src/main/java/random/meteor/Utils/MiscUtils.java
@@ -21,24 +21,8 @@ public class MiscUtils {
         long timeSinceLastSwap = currentTime - lastSwapTime;
         return timeSinceLastSwap >= 1000; // 1 second delay
     }
+
     public static boolean isFalling() {
         return (mc.player.isFallFlying() && mc.player.getSafeFallDistance() > 3) ? true : false;
-    }
-    public static int getFallDistance() {
-        if (!isFalling()) {
-            return 0;
-        }
-        Vec3d pos = mc.player.getPos();
-        Vec3d motion = mc.player.getVelocity();
-        double distance = 0.0;
-        while (distance < 256.0) { //TODOD: stop looping error
-            Vec3d targetPos = pos.add(motion.multiply(distance, distance, distance));
-            BlockHitResult result = mc.world.raycast(new RaycastContext(pos, targetPos, RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, mc.player));
-            if (result.getType() != HitResult.Type.MISS) {
-                return (int) Math.ceil(distance - result.getPos().distanceTo(pos));
-            }
-            distance += 0.5;
-        }
-        return 0;
     }
 }
