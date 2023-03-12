1f5ddf9fb96197bddbb982f3ef5af563e57cd62c
SplashAni <106507600+SplashAni@users.noreply.github.com>
2023-03-12 11:38:33 +0200

    :scroll: auto mine in chunkban

--- src/main/java/random/meteor/systems/modules/combat/AutoChunkBan.java
+++ src/main/java/random/meteor/systems/modules/combat/AutoChunkBan.java
@@ -3,12 +3,15 @@ package random.meteor.systems.modules.combat;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.friends.Friends;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import meteordevelopment.meteorclient.utils.player.FindItemResult;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.meteorclient.utils.world.BlockUtils;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import random.meteor.Main;
 import random.meteor.systems.modules.utils.CombatUtils;
 
@@ -33,7 +36,7 @@ public class AutoChunkBan extends Module {
     private final Setting<Boolean> ignoreFriends = sgGeneral.add(new BoolSetting.Builder()
             .name("ignore-friends")
             .description("doesn't ban u friends ")
-            .defaultValue(false)
+            .defaultValue(true)
             .build()
     );
     private final Setting<Integer> range = sgGeneral.add(new IntSetting.Builder()
@@ -44,6 +47,7 @@ public class AutoChunkBan extends Module {
             .sliderMax(30)
             .build()
     );
+
     private final Setting<Boolean> rotate = sgGeneral.add(new BoolSetting.Builder()
             .name("rotate")
             .description("wheather to rotate on place")
@@ -56,7 +60,28 @@ public class AutoChunkBan extends Module {
             .defaultValue(false)
             .build()
     );
-
+    private final Setting<Boolean> autoMine = sgGeneral.add(new BoolSetting.Builder()
+            .name("auto-mine")
+            .description("mines the shulker after being sorted")
+            .defaultValue(false)
+            .build()
+    );
+    private final Setting<Integer> startDelay = sgGeneral.add(new IntSetting.Builder()
+            .name("start-delay")
+            .description("delay before sorting")
+            .defaultValue(10)
+            .range(1,10)
+            .sliderMax(20)
+            .visible(autoMine::get)
+            .build()
+    );
+    private final Setting<Boolean> mineDebug = sgGeneral.add(new BoolSetting.Builder()
+            .name("mine-debug")
+            .description("send timer to chat")
+            .defaultValue(false)
+            .visible(autoMine::get)
+            .build()
+    );
     public AutoChunkBan() {
         super(Main.COMBAT, "auto-chunk-ban", "");
 
@@ -85,7 +110,20 @@ public class AutoChunkBan extends Module {
                 BlockPos targetBlockPos = target.getBlockPos();
                 BlockPos blockPos = targetBlockPos.north();
                 BlockUtils.place(blockPos, shulkerResult(), rotate.get(), 100, swing.get(), true);
+                autoMine(blockPos);
+            }
+        }
+    }
+    private void autoMine(BlockPos pos) {
+        Direction direction = mc.player.getHorizontalFacing().getOpposite();
 
+        mc.getNetworkHandler().sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.START_DESTROY_BLOCK, pos, direction));
+        for (int i = 1; i <= startDelay.get() + 1000; i++) {
+            if (mineDebug.get() && i % 100 == 0) {
+                info("Starting in: " + i);
+            }
+            if (i == startDelay.get() + 1000) {
+                mc.getNetworkHandler().sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.STOP_DESTROY_BLOCK, pos, direction));
             }
         }
     }
