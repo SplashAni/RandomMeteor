caa7fea974b4800c243e312295703fe33bd1e847
SplashAni <106507600+SplashAni@users.noreply.github.com>
2023-08-28 18:28:01 +0200

    finished calcs (hopefully)

---
+++ src/main/java/random/meteor/systems/modules/RM/PistonAura/InvalidPositionException.java
@@ -0,0 +1,8 @@
+package random.meteor.systems.modules.RM.PistonAura;
+
+public class InvalidPositionException extends Exception {
+    @Override
+    public void printStackTrace() {
+        super.printStackTrace();
+    }
+}
--- src/main/java/random/meteor/systems/modules/RM/PistonAura/PistonPositions.java
+++ src/main/java/random/meteor/systems/modules/RM/PistonAura/PistonPositions.java
@@ -4,6 +4,7 @@ import meteordevelopment.meteorclient.utils.world.BlockUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import random.meteor.systems.modules.RM.Blockpos;
 import random.meteor.systems.modules.utils.Utils;
 
@@ -15,35 +16,80 @@ import static meteordevelopment.meteorclient.utils.world.BlockUtils.canPlace;
 public class PistonPositions {
     boolean checkExisting;
     BlockPos pos;
+    /* returning poses*/
+    BlockPos pistonPos,redstonPos,basePos;
 
-    public Positions pistonPostion(BlockPos pos, boolean checkExisting) {
+    public Position pistonPostion(BlockPos pos, boolean checkExisting) {
         this.checkExisting = checkExisting;
         this.pos = pos;
 
-        return
     }
 
-    public BlockPos calcPosition() {
+    public Position calcPosition() {
 
-    }
+        BlockPos pp = getPistonPosition();
+        basePosition bp = getBasePosition();
 
-    public BlockPos poses() {
-        List<BlockPos> p = new ArrayList<>();
+        if(pp == null || bp == null) return null;
 
-        BlockPos[] offsets = {
-            new BlockPos(0, 0, -2), // north
-            new BlockPos(0, 0, 2),  // south
-            new BlockPos(-2, 0, 0), // west
-            new BlockPos(2, 0, 0)    // east
-        };
 
-        for (BlockPos poses : offsets) {
-            if (isValidPiston(pos)) p.add(poses);
+        List<BlockPos> redstonePoses = new ArrayList<>();
+
+
+        for(Direction d : Direction.values()){
+            if(canPlace(getPistonPosition().offset(d)))  redstonePoses.add(pistonPos);
         }
 
-        if (p.isEmpty() || p.size() > 2) return null;
+        if(redstonePoses.isEmpty() || redstonePoses.size() > 2){
+            return null;
+        } else {
+            return new Position(bp.createBase,bp.pos,pp,redstonePoses.get(0));
+        }
+    }
 
-        return p.get(0);
+    public BlockPos getPistonPosition() {
+        basePosition o = getBasePosition();
+
+        if (o == null) return null;
+
+        BlockPos p = switch (o.d) {
+            case NORTH -> new BlockPos(o.pos.getX(), o.pos.getY(), o.pos.getZ() - 1);
+            case SOUTH -> new BlockPos(o.pos.getX(), o.pos.getY(), o.pos.getZ() + 1);
+            case WEST -> new BlockPos(o.pos.getX() - 1, o.pos.getY(), o.pos.getZ());
+            case EAST -> new BlockPos(o.pos.getX() + 1, o.pos.getY(), o.pos.getZ());
+            default -> null;
+        };
+
+        return isValidPiston(p) ? p : null;
+    }
+
+
+
+    public basePosition getBasePosition(){
+        List<basePosition> p = new ArrayList<>();
+
+        for(Direction d : Direction.values()){
+            if(d == Direction.UP || d == Direction.DOWN) continue;
+            BlockPos p1  = pos.offset(d);
+
+            Block state = Utils.state(p1);
+
+            if(state == Blocks.OBSIDIAN || state == Blocks.BEDROCK){
+
+                p.add(new basePosition(p1,d,false));
+
+            } else if (state == Blocks.AIR){
+
+                p.add(new basePosition(p1,d,true));
+            }
+
+        }
+
+
+        if(p.isEmpty() || p.size() > 2) return null;
+
+        return p.get(1);
+
     }
 
     public boolean isValidPiston(BlockPos p) {
@@ -55,8 +101,13 @@ public class PistonPositions {
 
         return BlockUtils.canPlace(p);
     }
-    public record Positions(BlockPos obsidianPos,BlockPos pistonPos,BlockPos redstonePos){
+    public record Position(boolean createBase,BlockPos obsidianPos, BlockPos pistonPos, BlockPos redstonePos){
 
     }
+    public record basePosition(BlockPos pos, Direction d,boolean createBase){
+
+    }
+
+
 }
 
